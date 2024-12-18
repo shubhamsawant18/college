994434df5217db411cbe904936db9669c3a15ae0
shubhamsawant18 <shubhsawant124@gmail.com>
2024-12-18 21:03:45 +0530

    css added

--- src/components/RegistrationModal.js
+++ src/components/RegistrationModal.js
@@ -1,7 +1,6 @@
-// RegistrationModal.js
 import React from "react";
 import "../assets/styles/RegistrationModal.css";
-
+import { FaUser, FaEnvelope, FaPhoneAlt, FaCity, FaCheckCircle } from "react-icons/fa";
 
 const RegistrationModal = ({ show, onClose }) => {
   if (!show) return null;
@@ -13,35 +12,43 @@ const RegistrationModal = ({ show, onClose }) => {
           X
         </button>
         <div className="leadform-header">
-          <div className="lead-hdr-img rounded-circle">
-            <img
-              src="https://image-static.collegedunia.com/public/asset/img/cd-static-nr/leadform_logo.png?h=50&amp;w=50&amp;mode=stretch"
-              alt="Collegedunia Logo"
-            />
+          <div className="lead-hdr-img">
+            <img src="https://via.placeholder.com/60" alt="User Icon" />
           </div>
           <div className="lead-hdr-info">
             <h3>Register Now To Apply</h3>
             <p>Get details and latest updates</p>
           </div>
         </div>
-
         <form className="registration-form">
-          <input type="text" placeholder="Full Name *" required />
-          <input type="email" placeholder="Email Address *" required />
-          <div className="mobile-input-container">
+          <div className="input-group">
+            <FaUser className="react-icons" />
+            <input type="text" placeholder="Full Name *" required />
+          </div>
+          <div className="input-group">
+            <FaEnvelope className="react-icons" />
+            <input type="email" placeholder="Email Address *" required />
+          </div>
+          <div className="input-group mobile-input-container">
             <span className="country-code">+91</span>
+            <FaPhoneAlt className="react-icons" />
             <input type="tel" placeholder="Mobile Number *" required />
           </div>
-          <input type="text" placeholder="City You Live In *" required />
-          <select required>
-            <option value="">Course Interested In *</option>
-            <option value="BA">BA - Bachelors (Arts)</option>
-            {/* Add other courses here */}
-          </select>
+          <div className="input-group">
+            <FaCity className="react-icons" />
+            <input type="text" placeholder="City You Live In *" required />
+          </div>
+          <div className="input-group">
+            <select required>
+              <option value="">Course Interested In *</option>
+              <option value="BA">BA - Bachelors (Arts)</option>
+              {/* Add other courses here */}
+            </select>
+          </div>
           <div className="checkbox-container">
             <input type="checkbox" id="onlineCourse" />
             <label htmlFor="onlineCourse">
-              Looking For Online/Distance Course?
+              <FaCheckCircle size={18} color="#ff6600" /> Looking For Online/Distance Course?
             </label>
           </div>
           <button type="submit" className="submit-button">
@@ -49,7 +56,7 @@ const RegistrationModal = ({ show, onClose }) => {
           </button>
         </form>
         <p className="login-link">
-          Already Registered? <a href="/login">Click Here To Login</a>.
+          Already Registered? <a href="/login">Click Here To Login</a>
         </p>
       </div>
     </div>
