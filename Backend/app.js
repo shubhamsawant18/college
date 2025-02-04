799e14d020b8192622062985680751cf7b59c34d
shubhamsawant18 <shubhsawant124@gmail.com>
2025-02-04 16:44:41 +0530

    fixed everything predictors neet,cat,jeem,jeeadv,gate

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Ensure this is before your routes
+app.use(express.json()); 
 
 // Routes
 const userRoutes = require("./routes/userRoutes");
